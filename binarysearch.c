f77b12e5fd662a0d96fed9ff34dc97aaf2a6701f
zsyc <33007708+zsyc@users.noreply.github.com>
2020-03-08 18:00:29 +0100

    revise the recessive overflow error

--- binarysearch.c
+++ binarysearch.c
@@ -7,7 +7,7 @@ int binarysearch(int number){
 	int mid, start=0, end = LEN-1;
 	
 	while (start<=end){
-		mid = (start + end)/2;
+		mid = start + (end-start)/2;	// don't direct use (start + end)/2, overflow error
 		if (a[mid]<number) start=mid+1;
 		else if (a[mid]>number) end=mid-1;
 		else return mid;
@@ -16,7 +16,7 @@ int binarysearch(int number){
 }
 
 int main(void){
-	printf("%d\n", binarysearch(4));
+	printf("%d\n", binarysearch(2));
 	return 0;
 }
 
